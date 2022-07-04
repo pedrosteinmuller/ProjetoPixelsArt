b117c5099264b4f01a8ed0c8719c3cc9fa54d5a5
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-04 17:36:20 -0300

    resolucao requisito 6

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const colorsPallete = document.querySelectorAll(".color")
-const lista = ["#000","#f00","#00f","#008000"];
+const colorsPallete = document.querySelectorAll('.color');
+const lista = ['#000', '#f00', '#00f', '#008000'];
 
 function createColors() {
-    for(let index = 0; index < colorsPallete.length; index+= 1) {
-        colorsPallete[index].style.backgroundColor = lista[index];
-    }
+  for (let index = 0; index < colorsPallete.length; index += 1) {
+    colorsPallete[index].style.backgroundColor = lista[index];
+  }
 }
 
 createColors();
 
 //Criando o pixel board 5x5 dinamicamente
-
+let board = document.querySelector('#pixel-board');
 for (let index = 1; index <= 25; index += 1) {
-    let board = document.querySelector('#pixel-board');
-    let divs = document.createElement('div');
-    divs.classList.add('pixel');
-    divs.style.backgroundColor = '#fff';
-    board.appendChild(divs);
-  }
-  
+  let divs = document.createElement('div');
+  divs.classList.add('pixel');
+  divs.style.backgroundColor = '#fff';
+  board.appendChild(divs);
+}
+
