f6ada0d48c05a95e19f322b92bb6d8b9c1db2ed4
Pedro <pedrosteinmuller10105@hotmail.com>
2022-07-04 20:53:28 -0300

    resolucao requisito 7 e organizacao de alguns codigos dos arquivos

--- script.js
+++ script.js
@@ -18,3 +18,68 @@ for (let index = 1; index <= 25; index += 1) {
   board.appendChild(divs);
 }
 
+let colors = document.querySelectorAll('.color');
+
+for (let index = 0; index < colors.length; index += 1) {
+  colors[index].addEventListener('click', function (e) {
+    for (let a of colors) {
+      if (a.classList.contains('selected')) {
+        a.classList.remove('selected');
+      }
+    }
+    e.target.classList.add('selected');
+  });
+}
+
+let pixels = document.querySelectorAll('.pixel')
+
+for (let index = 0; index < pixels.length; index += 1) {
+
+}
+
+
+
+
+
+
+
+
+
+// outra forma de resolver o requisito 7, mas nao consegui raciocinar para finalizar, entao segui outra logica para resolucao
+// let selected = 'cor1';
+// let color1 = document.querySelector('.cor1');
+// let color2 = document.querySelector('.cor2');
+// let color3 = document.querySelector('.cor3');
+// let color4 = document.querySelector('.cor4');
+
+// color1.addEventListener('click', function() {
+//   color2.classList.remove('selected');
+//   color3.classList.remove('selected');
+//   color4.classList.remove('selected');
+//   color1.classList.add('selected');
+//   selected = 'cor1';
+// });
+
+// color2.addEventListener('click', function() {
+//     color1.classList.remove('selected');
+//     color3.classList.remove('selected');
+//     color4.classList.remove('selected');
+//     color2.classList.add('selected');
+//     selected = 'cor2';
+//   });
+
+//   color3.addEventListener('click', function() {
+//     color1.classList.remove('selected');
+//     color2.classList.remove('selected');
+//     color4.classList.remove('selected');
+//     color3.classList.add('selected');
+//     selected = 'cor3';
+//   });
+
+//   color3.addEventListener('click', function() {
+//     color1.classList.remove('selected');
+//     color2.classList.remove('selected');
+//     color3.classList.remove('selected');
+//     color4.classList.add('selected');
+//     selected = 'cor4';
+//   });
